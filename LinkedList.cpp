21355ffa52d72426eadc682a77ace66c1ded97f2
99003510 <rajathpd.pd@ltts.com>
2025-08-22 23:14:15 +0530

    Linked List print and create
    Signed-off-by: 99003510 <rajathpd.pd@ltts.com>

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -8,11 +8,36 @@ struct Node{
 
 }*head = NULL;
 
-void create(int A[], )
+void create(int A[], int n) {
+    int i;
+    struct Node* temp, *last;
+    head = new Node;
+    head->data = A[0];
+    head->next = NULL;
+    last = head;
+    for(i=1; i<n; i++) {
+        temp = new Node;
+        temp->data = A[i];
+        temp->next = NULL;
+        last->next = temp;
+        last = temp;
+    }
+
+}
+void Print_LL(struct Node* head) {
+    Node* curr = head;
+    while(curr != nullptr) {
+        cout<<curr->data<<endl;
+        curr = curr->next;
+    }
+}
 int main() {
 
+    int A[] = {23,56,43,87,99,12,8};
+    int n = sizeof(A)/sizeof(int);
+    create(A, n);
+    Print_LL(head);
     cout<<"Hello World!"<<endl;
-    cout<<"Added a line!!"<<endl;
-    cout<<"Added new features!"<<endl;
+    
     return 0;
 }
